3c2ba4dc7b210642285d64dc0903566e90195696
Giang D Doan <gdd33@cs.drexel.edu>
2019-01-26 23:55:33 -0500

    main js

---
+++ main.js
@@ -0,0 +1,25 @@
+
+function id(i, j) {
+    return document.getElementById("i" + i + "j"j);
+}
+function genTable() {
+    var i, j;
+    document.writeln('<table><tbody>');
+    for (i = 0; i < 50; i++) {
+        document.writeln('<tr>');
+        for (j = 0; j < 50; j++) {
+            document.writeln('<td align="center" id="i' + i + 'j' + j + '" style="width:8pt; height:8pt"></td>');
+        }
+        document.writeln('</tr>');
+    }
+    document.writeln('</tbody></table>');
+}
+function genFood() {
+    var x = random(50)
+    var y = random(50)
+    id(x, y).style.backgroundColor = "red";
+}
+function random(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}    
+
